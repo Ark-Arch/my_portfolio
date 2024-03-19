6bad11c9e0a7506d02e6863de2d4e11f171dbf09
David Ifeoluwa Agbemuko <davidagbemuko.ad@gmail.com>
2024-03-19 14:29:30 +0100

    seperated about-me page from about-projects page

--- website/routes.py
+++ website/routes.py
@@ -5,4 +5,11 @@ routes = Blueprint('routes', __name__)
 
 @routes.route('/')
 def home():
-    return "HELLO WORLD"
+    return render_template("home_page.html")
+
+@routes.route('/my-projects')
+def my_projects():
+    return render_template("my_projects.html")
+
+
+
